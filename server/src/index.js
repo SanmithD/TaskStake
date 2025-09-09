146d940a8b56493e782e06716416655adbfa84c0
SanmithD <sanmithdevadiga7@gmail.com>
2025-09-09 12:09:50 +0530

    changes 9

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://task-stake.vercel.app',
     credentials: true,
     methods: ['GET','PUT',"POST","DELETE","OPTIONS","PATCH"]
 }));
